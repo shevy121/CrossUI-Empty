5c0925e064c4b38d6d552a079a689d949ed99c22
shevy121 <141412170+shevy121@users.noreply.github.com>
2025-03-19 08:26:10 +0500

    Updated by CrossUI No-Code App Builder

--- App/js/123.js
+++ App/js/123.js
@@ -4,9 +4,21 @@ xui.Class('App.ColumnsNone25', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"ctl_pane10")
+                .setDock("fill")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput23")
+                .setLeft("23.61904761904762em")
+                .setTop("11.428571428571429em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Counter")
+                .setType("counter")
             );
             
             return children;
