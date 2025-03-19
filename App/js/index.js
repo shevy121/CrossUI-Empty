ed6e3a8af7b05769be2dd7bf728e2d6857fcef07
shevy121 <141412170+shevy121@users.noreply.github.com>
2025-03-19 08:25:51 +0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -32,6 +32,51 @@ xui.Class('App', 'xui.Module',{
                 .setMultiLines(true)
             );
             
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input12")
+                .setLeft("16.761904761904763em")
+                .setTop("12.952380952380953em")
+                .setWidth("19.504761904761907em")
+                .setHeight("3.123809523809524em")
+                .setLabelSize("8em")
+                .setLabelCaption("Receiver")
+                .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input13")
+                .setLeft("16.761904761904763em")
+                .setTop("18.285714285714285em")
+                .setWidth("23.314285714285713em")
+                .setHeight("8.457142857142857em")
+                .setLabelSize("8em")
+                .setLabelCaption("Message")
+                .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button6")
+                .setLeft("28.19047619047619em")
+                .setTop("32em")
+                .setWidth("5.257142857142857em")
+                .setHeight("2.8190476190476192em")
+                .setCaption("Send")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "page",
+                        "target" : "App.123",
+                        "args" : [true],
+                        "method" : "switch",
+                        "event" : 1
+                    },
+                    "_xui_ui_button6_onclick"
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
@@ -40,6 +85,17 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        /**
+         * Fired when user click it
+         * @method onClick [xui.UI.Button event]
+         * @param {xui.UIProfile.} profile  The current control's profile object
+         * @param {Event} e , Dom event object
+         * @param {Element.xui} src  id or Dom Element
+         * @param {} value  Object
+        */
+        _xui_ui_button6_onclick:function(profile, e, src, value){
+            var ns = this, uictrl = profile.boxing();
         }
         /*,
         // To determine how properties affects this module
